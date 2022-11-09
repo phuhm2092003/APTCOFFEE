0ecaadec5ba3b3302d490b4fb5cd9a1d2590580e
ASUS <tinnguyen29112003@gmail.com>
2022-11-10 00:09:21 +0700

    model

--- app/src/main/java/fpt/edu/aptcoffee/model/HangHoa.java
+++ app/src/main/java/fpt/edu/aptcoffee/model/HangHoa.java
@@ -1,2 +1,92 @@
-package fpt.edu.aptcoffee.model;public class HangHoa {
+package fpt.edu.aptcoffee.model;
+
+import androidx.annotation.NonNull;
+
+import java.util.Arrays;
+
+public class HangHoa {
+    private int maHangHoa;
+    private String tenHangHoa;
+    private byte[] hinhAnh;
+    private int giaTien;
+    private int maLoai;
+    private int trangThai;
+
+    public HangHoa() {
+    }
+
+    public HangHoa(String tenHangHoa, byte[] hinhAnh) {
+        this.tenHangHoa = tenHangHoa;
+        this.hinhAnh = hinhAnh;
+    }
+
+    public HangHoa(int maHangHoa, String tenHangHoa, byte[] hinhAnh, int giaTien, int maLoai, int trangThai) {
+        this.maHangHoa = maHangHoa;
+        this.tenHangHoa = tenHangHoa;
+        this.hinhAnh = hinhAnh;
+        this.giaTien = giaTien;
+        this.maLoai = maLoai;
+        this.trangThai = trangThai;
+    }
+
+    public int getMaHangHoa() {
+        return maHangHoa;
+    }
+
+    public void setMaHangHoa(int maHangHoa) {
+        this.maHangHoa = maHangHoa;
+    }
+
+    public String getTenHangHoa() {
+        return tenHangHoa;
+    }
+
+    public void setTenHangHoa(String tenHangHoa) {
+        this.tenHangHoa = tenHangHoa;
+    }
+
+    public byte[] getHinhAnh() {
+        return hinhAnh;
+    }
+
+    public void setHinhAnh(byte[] hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+
+    public int getGiaTien() {
+        return giaTien;
+    }
+
+    public void setGiaTien(int giaTien) {
+        this.giaTien = giaTien;
+    }
+
+    public int getMaLoai() {
+        return maLoai;
+    }
+
+    public void setMaLoai(int maLoai) {
+        this.maLoai = maLoai;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "HangHoa{" +
+                "maHangHoa=" + maHangHoa +
+                ", tenHangHoa='" + tenHangHoa + '\'' +
+                ", hinhAnh=" + Arrays.toString(hinhAnh) +
+                ", giaTien=" + giaTien +
+                ", maLoai=" + maLoai +
+                ", trangThai=" + trangThai +
+                '}';
+    }
 }
--- app/src/main/java/fpt/edu/aptcoffee/model/HoaDon.java
+++ app/src/main/java/fpt/edu/aptcoffee/model/HoaDon.java
@@ -1,2 +1,81 @@
-package fpt.edu.aptcoffee.model;public class HoaDon {
+package fpt.edu.aptcoffee.model;
+
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+
+public class HoaDon {
+    private int maHoaDon;
+    private int maBan;
+    private int maNguoiDung;
+    private Date gioVao;
+    private Date gioRa;
+
+    public HoaDon() {
+    }
+
+    public HoaDon(Date gioVao, Date gioRa) {
+        this.gioVao = gioVao;
+        this.gioRa = gioRa;
+    }
+
+    public HoaDon(int maHoaDon, int maBan, int maNguoiDung, Date gioVao, Date gioRa) {
+        this.maHoaDon = maHoaDon;
+        this.maBan = maBan;
+        this.maNguoiDung = maNguoiDung;
+        this.gioVao = gioVao;
+        this.gioRa = gioRa;
+    }
+
+    public int getMaHoaDon() {
+        return maHoaDon;
+    }
+
+    public void setMaHoaDon(int maHoaDon) {
+        this.maHoaDon = maHoaDon;
+    }
+
+    public int getMaBan() {
+        return maBan;
+    }
+
+    public void setMaBan(int maBan) {
+        this.maBan = maBan;
+    }
+
+    public int getMaNguoiDung() {
+        return maNguoiDung;
+    }
+
+    public void setMaNguoiDung(int maNguoiDung) {
+        this.maNguoiDung = maNguoiDung;
+    }
+
+    public Date getGioVao() {
+        return gioVao;
+    }
+
+    public void setGioVao(Date gioVao) {
+        this.gioVao = gioVao;
+    }
+
+    public Date getGioRa() {
+        return gioRa;
+    }
+
+    public void setGioRa(Date gioRa) {
+        this.gioRa = gioRa;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "HoaDon{" +
+                "maHoaDon=" + maHoaDon +
+                ", maBan=" + maBan +
+                ", maNguoiDung=" + maNguoiDung +
+                ", gioVao=" + gioVao +
+                ", gioRa=" + gioRa +
+                '}';
+    }
 }
