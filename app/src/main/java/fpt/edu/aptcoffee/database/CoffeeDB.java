2b86589d43b53528778c15fc0db086fb67a544f4
DELL <hominphuzxc@gmail.com>
2022-11-10 18:52:35 +0700

    Phu Update Database

--- app/src/main/java/fpt/edu/aptcoffee/database/CoffeeDB.java
+++ app/src/main/java/fpt/edu/aptcoffee/database/CoffeeDB.java
@@ -7,14 +7,49 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class CoffeeDB extends SQLiteOpenHelper {
     public static final String DB_NAME = "APTCoffee";
     public static final int DB_VERSION = 1;
-    public static final String TABLE_BAN = "CREATE TABLE BAN(maBan INTEGER PRIMARY KEY AUTOINCREMENT, trangThai INTEGER NOT NULL)";
-    public static final String TABLE_NGUOIDUNG = "CREATE TABLE NGUOIDUNG(maNguoiDung TEXT PRIMARY KEY, hoVaTen TEXT NOT NULL, hinhAnh BLOB, ngaySinh DATE NOT NULL, email TEXT NOT NULL, chucVu TEXT NOT NULL, gioiTinh TEXT NOT NULL, matKhau TEXT)";
-    public static final String TABLE_LOAIHANG = "CREATE TABLE LOAIHANG(maLoai INTEGER PRIMARY KEY AUTOINCREMENT, hinhAnh BLOB , tenLoai TEXT NOT NULL)";
-    public static final String TABLE_HOADON = "CREATE TABLE HOADON(maHoaDon INTEGER PRIMARY KEY AUTOINCREMENT, maBan INTEGER REFERENCES BAN(maBan), maNguoiDung TEXT REFERENCES NGUOIDUNG(maNguoiDung), gioVao DATE NOT NULL, gioRa DATE NOT NULL)";
-    public static final String TABLE_HANGHOA = "CREATE TABLE HANGHOA( maHangHoa INTEGER PRIMARY KEY AUTOINCREMENT, hinhAnh BLOB, tenHangHoa TEXT NOT NULL, giaTien INTEGER NOT NULL, maLoai INTEGER REFERENCES LOAIHANG(maLoai), trangThai INTEGER NOT NULL)";
-    public static final String TABLE_HOADONCHITIET = "CREATE TABLE HOADONCHITIET( maHDCT INTEGER PRIMARY KEY AUTOINCREMENT, maHoaDon INTEGER REFERENCES HOADON(maHoaDon), maHangHoa INTEGER REFERENCES HANGHOA(maHangHoa), soLuong INTEGER NOT NULL, giaTien INTEGER NOT NULL, ghiChu TEXT, ngayXuatHoaDon DATE NOT NULL)";
+    public static final String TABLE_BAN = "CREATE TABLE BAN(" +
+            "maBan INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "trangThai INTEGER NOT NULL)";
 
-    public CoffeeDB(Context context){
+    public static final String TABLE_NGUOIDUNG = "CREATE TABLE NGUOIDUNG(" +
+            "maNguoiDung TEXT PRIMARY KEY," +
+            "hoVaTen TEXT NOT NULL," +
+            "hinhAnh BLOB," +
+            "ngaySinh DATE NOT NULL," +
+            "email TEXT NOT NULL," +
+            "chucVu TEXT NOT NULL," +
+            "gioiTinh TEXT NOT NULL," +
+            "matKhau TEXT NOT NULL)";
+
+    public static final String TABLE_LOAIHANG = "CREATE TABLE LOAIHANG(" +
+            "maLoai INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "hinhAnh BLOB ," +
+            "tenLoai TEXT NOT NULL)";
+
+    public static final String TABLE_HOADON = "CREATE TABLE HOADON(" +
+            "maHoaDon INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "maBan INTEGER REFERENCES BAN(maBan)," +
+            "maNguoiDung TEXT REFERENCES NGUOIDUNG(maNguoiDung)," +
+            "gioVao DATE NOT NULL," +
+            "gioRa DATE NOT NULL)";
+
+    public static final String TABLE_HANGHOA = "CREATE TABLE HANGHOA(" +
+            "maHangHoa INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "hinhAnh BLOB," +
+            "tenHangHoa TEXT NOT NULL," +
+            "giaTien INTEGER NOT NULL, " +
+            "maLoai INTEGER REFERENCES LOAIHANG(maLoai)," +
+            "trangThai INTEGER NOT NULL)";
+
+    public static final String TABLE_HOADONCHITIET = "CREATE TABLE HOADONCHITIET(" +
+            "maHDCT INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "maHoaDon INTEGER REFERENCES HOADON(maHoaDon)," +
+            "maHangHoa INTEGER REFERENCES HANGHOA(maHangHoa)," +
+            "soLuong INTEGER NOT NULL," +
+            "giaTien INTEGER NOT NULL, ghiChu TEXT," +
+            "ngayXuatHoaDon DATE NOT NULL)";
+
+    public CoffeeDB(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
@@ -36,7 +71,7 @@ public class CoffeeDB extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        if (oldVersion != newVersion){
+        if (oldVersion != newVersion) {
             sqLiteDatabase.execSQL("DROP TABLE IF EXISTS BAN");
             sqLiteDatabase.execSQL("DROP TABLE IF EXISTS NGUOIDUNG");
             sqLiteDatabase.execSQL("DROP TABLE IF EXISTS LOAIHANG");
