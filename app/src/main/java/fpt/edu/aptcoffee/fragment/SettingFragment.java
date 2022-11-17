b7cf6bca6d2f17a5e0131fbd26d0ad4c9142d48d
DELL <hominphuzxc@gmail.com>
2022-11-18 05:03:40 +0700

    Xong DanhGia

--- app/src/main/java/fpt/edu/aptcoffee/fragment/SettingFragment.java
+++ app/src/main/java/fpt/edu/aptcoffee/fragment/SettingFragment.java
@@ -3,10 +3,12 @@ package fpt.edu.aptcoffee.fragment;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +16,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -26,6 +31,7 @@ import fpt.edu.aptcoffee.model.NguoiDung;
 import fpt.edu.aptcoffee.ui.DoiMatKhauActivity;
 import fpt.edu.aptcoffee.ui.SignInActivity;
 import fpt.edu.aptcoffee.utils.MyToast;
+import pl.droidsonroids.gif.GifImageView;
 
 public class SettingFragment extends Fragment implements View.OnClickListener {
     TextView tvDanhGia, tvLienHe, tvThietLapTaiKhoan, tvDoiMatKhuau, tvDangXuat, tvTenNguoiDung, tvChucVu;
@@ -65,39 +71,18 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
     }
 
     private void getInfoNguoiDung() {
+        // Lấy mã người dùng
         String maNguoiDung = Objects.requireNonNull(mainActivity).getKeyUser();
+        // Lấy người dùng theo mã mã người dùng
         NguoiDung nguoiDung = nguoiDungDAO.getByMaNguoiDung(maNguoiDung);
+        // Tạo Bitmap từ mảng Byte[] HinhAnh
         Bitmap bitmap = BitmapFactory.decodeByteArray(nguoiDung.getHinhAnh(),
                 0,
                 nguoiDung.getHinhAnh().length);
+        // Gán dữ liệu cho View
         tvTenNguoiDung.setText(nguoiDung.getHoVaTen());
         tvChucVu.setText(nguoiDung.getChucVu());
         civHinhAnh.setImageBitmap(bitmap);
-    }
-
-    private void onpenDoiMatKhauAcitvity() {
-        startActivity(new Intent(getContext(), DoiMatKhauActivity.class));
-        ((Activity) requireContext()).overridePendingTransition(R.anim.anim_in_right, R.anim.anim_out_left);
-    }
-
-    private void logoutSytem() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setMessage("Bạn có muốn đăng xuất?");
-        builder.setPositiveButton("Đăng xuất", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                startActivity(new Intent(getContext(), SignInActivity.class));
-                ((Activity) requireContext()).overridePendingTransition(R.anim.anim_in_left, R.anim.anim_out_right);
-            }
-        });
-        builder.setNegativeButton("Huỷ", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -105,7 +90,7 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tvDanhGia:
-                MyToast.successful(getContext(), "Đánh giá");
+                showDialogDanhGia();
                 break;
             case R.id.tvLienHe:
                 MyToast.successful(getContext(), "Liên hệ");
@@ -120,5 +105,84 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
                 logoutSytem();
                 break;
         }
+    }
+
+    private void onpenDoiMatKhauAcitvity() {
+        startActivity(new Intent(getContext(), DoiMatKhauActivity.class));
+        ((Activity) requireContext()).overridePendingTransition(R.anim.anim_in_right, R.anim.anim_out_left);
+    }
+
+    private void logoutSytem() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage("Bạn có muốn đăng xuất?");
+        builder.setPositiveButton("Đăng xuất", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                openSignInActivity();
+            }
+        });
+        builder.setNegativeButton("Huỷ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void openSignInActivity() {
+        startActivity(new Intent(getContext(), SignInActivity.class));
+        ((Activity) requireContext()).overridePendingTransition(R.anim.anim_in_left, R.anim.anim_out_right);
+    }
+
+
+    private void showDialogDanhGia() {
+        // Tạo view mới từ package layout
+        View viewDialog = LayoutInflater.from(getContext()).inflate(R.layout.layout_danh_gia, null);
+        // Ánh xạ View từ viewDialog
+        RatingBar ratingBar = viewDialog.findViewById(R.id.rtbDanhGia);
+        Button btnDanhGia = viewDialog.findViewById(R.id.btnDanhGia);
+        TextView tvBoQua = viewDialog.findViewById(R.id.tvBoQua);
+        GifImageView gifImageView = viewDialog.findViewById(R.id.imgGif);
+        // Tạo mới dialog
+        Dialog dialog = new Dialog(getContext());
+        // Gán Backgound trong suốt cho dialog
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        // Gán view cho dialog
+        dialog.setContentView(viewDialog);
+        // Khởi tạo chiều rộng và chiều cao cho dialog
+        int width = (int) (getResources().getDisplayMetrics().widthPixels * 0.90);
+        int height = WindowManager.LayoutParams.WRAP_CONTENT;
+        // Gán Size cho dialog
+        dialog.getWindow().setLayout(width, height);
+        // Sự kiện bỏ qua
+        tvBoQua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        // Sự kiện thay đổi số lượng sao đánh giá
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                if (ratingBar.getRating() <= 3) {
+                    // số lượng sao nhỏ hơn hoặc bằng 3
+                    gifImageView.setImageResource(R.drawable.git_sad);
+                } else {
+                    // số lượng sao lớn hơn 3
+                    gifImageView.setImageResource(R.drawable.gif_danh_gia);
+                }
+            }
+        });
+        // Sự kiện đánh giá
+        btnDanhGia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyToast.successful(getContext(), "Đã đánh giá " + ratingBar.getRating() + " sao");
+            }
+        });
+        dialog.show();
     }
 }
