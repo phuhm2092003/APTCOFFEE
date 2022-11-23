90485c022854daac8a5e7912b2fc80a3a54f3e4d
DELL <hominphuzxc@gmail.com>
2022-11-24 01:22:44 +0700

    design Loại Hàng

--- app/src/main/java/fpt/edu/aptcoffee/ui/LoaiThucUongActivity.java
+++ app/src/main/java/fpt/edu/aptcoffee/ui/LoaiThucUongActivity.java
@@ -1,16 +1,47 @@
 package fpt.edu.aptcoffee.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 import fpt.edu.aptcoffee.R;
+import fpt.edu.aptcoffee.model.LoaiHang;
 
 public class LoaiThucUongActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loai_thuc_uong);
+        initToolBar();
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void initToolBar() {
+        toolbar = findViewById(R.id.toolbar_loai);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.anim_in_left, R.anim.anim_out_right);
     }
 }
