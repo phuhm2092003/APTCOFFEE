864d3b486f74d1ad0fea4eb28c7f3a8ffa43a87e
DELL <hominphuzxc@gmail.com>
2022-12-09 13:36:58 +0700

    Cleand DoanhThuActivity

--- app/src/main/java/fpt/edu/aptcoffee/ui/DoanhThuActivity.java
+++ app/src/main/java/fpt/edu/aptcoffee/ui/DoanhThuActivity.java
@@ -43,39 +43,38 @@ public class DoanhThuActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     private void getDoangThuThang() {
         Calendar calendar = Calendar.getInstance();
-        int moth = calendar.get(Calendar.MONTH);
-        int year = calendar.get(Calendar.YEAR);
-        calendar.set(year, moth, 1);
-        Date tuNgay = calendar.getTime();
 
-        int dateTuNgay = getDate(moth, year);
-        calendar.set(year, moth, dateTuNgay);
+        int monthNow = calendar.get(Calendar.MONTH);
+        int yearNow = calendar.get(Calendar.YEAR);
+        // date: 01-monthNow-yearNow
+        calendar.set(yearNow, monthNow, 1);
+        Date tuNgay = calendar.getTime();
+        // date : getDate()-monthNow-yearNow
+        calendar.set(yearNow, monthNow, getDate(monthNow, yearNow));
         Date denNgay = calendar.getTime();
-        tvThang.setText("Tháng "+(moth + 1));
-        tvDoanhThuThang.setText(hoaDonChiTietDAO.getDTThangNam(XDate.toStringDate(tuNgay),
+        tvThang.setText("Tháng " + (monthNow + 1));
+        tvDoanhThuThang.setText(hoaDonChiTietDAO.getDTThangNam(
+                XDate.toStringDate(tuNgay),
                 XDate.toStringDate(denNgay)) + "VND");
     }
 
+    // Trả về số ngày theo tháng và năm nhuận
     private int getDate(int moth, int year) {
         switch (moth) {
-            case 0:
-            case 2:
-            case 4:
-            case 6:
-            case 7:
-            case 9:
-            case 11:
-                // tháng 12
+            case 0: // tháng 1
+            case 2: // tháng 3
+            case 4: // tháng 5
+            case 6: // tháng 7
+            case 7: // tháng 8
+            case 9: // tháng 9
+            case 11:// tháng 12
                 return 31;
-
-            case 3:
-            case 5:
-            case 8:
-            case 10:
-                //tháng 11
+            case 3: // tháng 4
+            case 5: // tháng 6
+            case 8: // tháng 9
+            case 10: // tháng 11
                 return 30;
-            case 1:
-                // tháng 2
+            case 1:// tháng 2
                 if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
                     return 29;
                 } else {
@@ -85,26 +84,30 @@ public class DoanhThuActivity extends AppCompatActivity {
         return 0;
     }
 
+    // Tính doanh thu theo năm
     @SuppressLint("SetTextI18n")
     private void getDoanhThuNam() {
         Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        calendar.set(year, 0, 1);
+        int yearNow = calendar.get(Calendar.YEAR);
+        // date: 01-01-yearNow
+        calendar.set(yearNow, 0, 1);
         Date tuNgay = calendar.getTime();
-
-        calendar.set(year, 11, 31);
+        // date: 31-12-yearNow
+        calendar.set(yearNow, 11, 31);
         Date denNgay = calendar.getTime();
+
         tvDoanhThuNam.setText(hoaDonChiTietDAO.getDTThangNam(
                 XDate.toStringDate(tuNgay),
                 XDate.toStringDate(denNgay)) + "VND"
         );
     }
 
+    // Tính doang thu theo ngày
     @SuppressLint("SetTextI18n")
     private void getDoanhThuNgay() {
         Date dateNow = Calendar.getInstance().getTime();
-        int doanhThuNgay = hoaDonChiTietDAO.getDoanhThuNgay(XDate.toStringDate(dateNow));
-        tvDoanhThuNgay.setText(doanhThuNgay + "VND");
+        int caculatorDoanhthu = hoaDonChiTietDAO.getDoanhThuNgay(XDate.toStringDate(dateNow));
+        tvDoanhThuNgay.setText(caculatorDoanhthu + "VND");
     }
 
     private void initView() {
